d021375aa3abf3c16f0392a31381ca973717df1f
Seth Watson <seth.watson1@gmail.com>
2018-06-17 13:15:46 -0400

    added header handling to app.js

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products');
 app.use('/contact')
