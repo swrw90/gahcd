2b9f6548c709ebada989216de5fc9235d31fce87
Seth Watson <seth.watson1@gmail.com>
2018-06-17 13:11:51 -0400

    added endpoints for routes to app.js

--- app.js
+++ app.js
@@ -4,4 +4,25 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+app.use('/products');
+app.use('/contact')
+app.use('user');
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    console.log('Error: Not Found');
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        }
+    });
+});
+
 module.exports = app;
