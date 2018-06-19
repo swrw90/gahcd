978e801b9bf09fed4a2f986f2b27fb01a8109842
Seth Watson <seth.watson1@gmail.com>
2018-06-18 20:28:11 -0400

    created products route

---
+++ api/routes/products.js
@@ -0,0 +1,8 @@
+const express = require('require');
+const router = express.Router();
+const mongoose = require('mongoose');
+const ProductsController = require('../controllers/products');
+
+router.get('/', ProductsController.get_all_products);
+
+module.exports = router;
