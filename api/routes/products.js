ee350253ac391bd5195fcf31e15c921f336d035b
Seth Watson <seth.watson1@gmail.com>
2018-06-25 17:41:23 -0400

    setup multer and fileFilter()

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,33 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const ProductsController = require('../controllers/products');
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' ||  file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
 router.get('/', ProductsController.get_all_products);
 
 module.exports = router;
