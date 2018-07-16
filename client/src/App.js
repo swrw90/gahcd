4fdb5efb5772ffb8a0895fea428c3ca1bd6bf48e
Seth Watson <seth.watson1@gmail.com>
2018-07-16 13:32:49 -0400

    sass is working, began build on photo-grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainNav from './components/MainNav/MainNav';
+import PhotoGrid from './components/PhotoGrid/PhotoGrid';
+
 class App extends Component {
   render() {
     return (
       <div>
         <MainNav />
+        <PhotoGrid />
       </div>
     );
   }
